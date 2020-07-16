34b06a2e16edf49f1785a0046712a4162b466f09
ZionTk6k <68042118+ZionTk6k@users.noreply.github.com>
2020-07-17 08:43:24 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,29 @@
 input.onButtonPressed(Button.A, function () {
-    led.unplot(posX, 0)
-    posX = Math.max(0, posX - 0)
+    led.unplot(posX, posY)
+    posX += -1
+    if (posX < 0) {
+        posX = 4
+        posY += -1
+        if (posY < 0) {
+            posY = 4
+        }
+    }
+    led.plot(posX, posY)
 })
 input.onButtonPressed(Button.B, function () {
-	
+    led.unplot(posX, posY)
+    posX += 1
+    if (posX > 4) {
+        posX = 0
+        posY += 1
+        if (posY > 4) {
+            posY = 0
+        }
+    }
+    led.plot(posX, posY)
 })
+let posY = 0
 let posX = 0
-posX = 0
-basic.forever(function () {
-    led.plot(Math.constrain(posX, 0, 4), 0)
-})
+posX = 2
+posY += 2
+led.plot(posX, posY)
